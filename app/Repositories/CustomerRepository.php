62bce76ff14fd760df037c949474b8a5c9d7b80d
Fatih <64631921+fatihavci06@users.noreply.github.com>
2023-12-11 19:07:57 +0300

    test

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -18,29 +18,15 @@ class CustomerRepository implements CustomerRepositoryInterface {
 
         $customer = new Customer($data);
 
-        if (!empty($data['photo'])) {
-            $customer->photo = Storage::putFile('images', $data['photo']);
-        }
-
         return $customer->save();
 
     }
 
     public function update($id, array $data) {
+
         $customer = Customer::findOrFail($id);
 
-        // Eğer photo değeri boş değilse ve yeni bir dosya yüklenmişse
-        if (!empty($data['photo'])) {
-            // Yeni dosyayı yükle
-            $customer->photo = Storage::putFile('images', $data['photo']);
-        }
-        $excludedKey = 'photo';
-
-        // $excludedKey anahtarını hariç tutarak yeni bir dizi oluştur
-        $filteredData = array_filter($data, function ($key) use ($excludedKey) {
-            return $key !== $excludedKey;
-        }, ARRAY_FILTER_USE_KEY);
-        return $customer->update($filteredData);
+        return $customer->update($data);
 
     }
 
--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -1,37 +1,51 @@
 <?php
+
 namespace App\Services;
 
 use App\Models\Customer;
 use App\Repositories\CustomerRepositoryInterface;
 use Illuminate\Support\Facades\Storage;
 
-class CustomerService {
+class CustomerService
+{
     protected $customerRepository;
 
-    public function __construct(CustomerRepositoryInterface $customerRepository) {
+    public function __construct(CustomerRepositoryInterface $customerRepository)
+    {
         $this->customerRepository = $customerRepository;
     }
 
-    public function getAllCustomers() {
+    public function getAllCustomers()
+    {
         return $this->customerRepository->getAll();
     }
 
-    public function getCustomerById($id) {
+    public function getCustomerById($id)
+    {
         return $this->customerRepository->find($id);
     }
 
-    public function createCustomer(array $data) {
+    public function createCustomer(array $data)
+    {
+        if (!empty($data['photo'])) {
+            $data['photo'] = Storage::putFile('images', $data['photo']);
 
+        }
         return $this->customerRepository->create($data);
     }
 
-    public function updateCustomer($id, array $data) {
+    public function updateCustomer($id, array $data)
+    {
+        if (!empty($data['photo'])) {
+            $data['photo'] = Storage::putFile('images', $data['photo']);
+
+        }
 
         return $this->customerRepository->update($id, $data);
     }
 
-    public function deleteCustomer($id) {
+    public function deleteCustomer($id)
+    {
         return $this->customerRepository->delete($id);
     }
 }
-
